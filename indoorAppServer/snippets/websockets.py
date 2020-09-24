1ae58e2b02a2343b898f341d9a65a842d620ce0d
Rui Pedro Porfirio <r.porfirio@campus.fct.unl.pt>
2020-09-24 09:55:23 +0100

    changed crossbar config

--- indoorAppServer/snippets/websockets.py
+++ indoorAppServer/snippets/websockets.py
@@ -14,7 +14,7 @@ import os
 
 def publish(username,position):
     payload = {'username': username,'position':position}
-    requests.post("http://127.0.0.1:9000/notify",
+    requests.post("http://127.0.0.1:$PORT/notify",
                   json={
                       'topic': 'onLocationUpdate',
                       'args': [payload]
