c1cb8fb7e8cadcfbd38fa3603020434af7ba3e52
Rui Pedro Porfirio <r.porfirio@campus.fct.unl.pt>
2020-09-24 15:35:56 +0100

    changed crossbar

--- indoorAppServer/snippets/websockets.py
+++ indoorAppServer/snippets/websockets.py
@@ -1,22 +1,10 @@
-from autobahn.twisted import sleep
-from autobahn.twisted.wamp import ApplicationSession
 import requests
-
-from django.db import models
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from twisted.internet.defer import inlineCallbacks
-from autobahn.twisted.wamp import ApplicationSession, ApplicationRunner
-from os import environ
-import sys
-import os
 
 
 def publish(username,position):
     print('Publishing new message')
     payload = {'username': username,'position':position}
-    print('Retrieving Port stated by heroku')
-    response = requests.post("http://indoorlocationapp.herokuapp.com/notify",
+    response = requests.post("http://localhost/notify",
                   json={
                       'topic': 'onLocationUpdate',
                       'args': [payload]
