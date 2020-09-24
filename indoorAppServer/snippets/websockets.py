791b6ac6c44093408edcd65ffb00b1a035d3e91a
Rui Pedro Porfirio <r.porfirio@campus.fct.unl.pt>
2020-09-24 16:49:33 +0100

    changed crossbar

--- indoorAppServer/snippets/websockets.py
+++ indoorAppServer/snippets/websockets.py
@@ -4,10 +4,8 @@ import requests
 def publish(username,position):
     print('Publishing new message')
     payload = {'username': username,'position':position}
-    response = requests.post("http://127.0.0.1/notify",
+    response = requests.post("http://indoorlocationapp.herokuapp.com/notify",
                   json={
                       'topic': 'onLocationUpdate',
                       'args': [payload]
                   })
-    print(str(response))
-    return 200
