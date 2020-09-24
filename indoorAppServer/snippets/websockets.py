1dc110a6b69f21ce4249e52b0d59c6aac46ec555
Rui Pedro Porfirio <r.porfirio@campus.fct.unl.pt>
2020-09-24 19:04:52 +0100

    added crossbar websocket

--- indoorAppServer/snippets/websockets.py
+++ indoorAppServer/snippets/websockets.py
@@ -5,9 +5,7 @@ import os
 def publish(username,position):
     print('Publishing new message')
     payload = {'username': username,'position':position}
-    port = os.environ.get("PORT")
-    print('Heroku port: ' + str(port))
-    response = requests.post("http://127.0.0.1:"+port+"/notify",
+    response = requests.post("http://127.0.0.1/notify",
                   json={
                       'topic': 'onLocationUpdate',
                       'args': [payload]
