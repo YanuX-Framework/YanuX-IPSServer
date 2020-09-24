d6c4e2e1ab865e68a2d4d85c8be5a593f84b401a
Rui Pedro Porfirio <r.porfirio@campus.fct.unl.pt>
2020-09-24 12:03:37 +0100

    problem with ws

--- indoorAppServer/snippets/websockets.py
+++ indoorAppServer/snippets/websockets.py
@@ -16,7 +16,7 @@ def publish(username,position):
     print('Publishing new message')
     payload = {'username': username,'position':position}
     print('Retrieving Port stated by heroku')
-    requests.post("http://127.0.0.1/notify",
+    requests.post("https://indoorlocationapp.herokuapp.com/notify",
                   json={
                       'topic': 'onLocationUpdate',
                       'args': [payload]
