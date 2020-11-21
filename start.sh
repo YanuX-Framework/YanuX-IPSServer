3714e82117ad97de22104c060f8c62a8a0252341
Pedro Albuquerque Santos <petersaints@gmail.com>
2020-11-21 02:47:07 +0000

    - Enabled Multi-threading

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 crossbar start &
-gunicorn Server.wsgi --bind 0.0.0.0:3100 --preload --workers=1 --max-requests=250 --max-requests-jitter=50
+gunicorn Server.wsgi --bind 0.0.0.0:3100 --preload --worker-class=gthread --workers=1 --threads=4 --max-requests=250 --max-requests-jitter=50
