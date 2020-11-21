605ac5350388e27ede1a86d447f191e2ad7cf1a1
Pedro Albuquerque Santos <petersaints@gmail.com>
2020-11-21 20:22:29 +0000

    I couldn't get Heroku "Classic" Python mode to work again. Definitely moved to Docker.
    With this new setup where I run crossbar and gunicorn separately and make crossbar reverse proxy into gunicorn, I'm unable to get Heroku "Classic" Python mode to work properly. Therefore, I'm switching to a Docker container which works without issue.

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: crossbar start && gunicorn Server.wsgi --bind 0.0.0.0:3100 --preload --worker-class=gthread --workers=2 --threads=2 --max-requests=250 --max-requests-jitter=50
--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 crossbar start &
-gunicorn Server.wsgi --bind 0.0.0.0:3100 --preload --worker-class=gthread --workers=2 --threads=2 --max-requests=250 --max-requests-jitter=50
+gunicorn Server.wsgi --bind 0.0.0.0:3100 --preload --worker-class=gthread --workers=2 --threads=2 --max-requests=500 --max-requests-jitter=50
