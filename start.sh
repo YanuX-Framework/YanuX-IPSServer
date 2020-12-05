afee0ba9b9264010d8e9b8806451978b1840fe0a
Pedro Albuquerque Santos <petersaints@gmail.com>
2020-12-05 02:15:34 +0000

    - Improving Requirements + Docker Setup Part 6

--- start.sh
+++ start.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-export PORT=${VARIABLE:-3101}
+if [ -z "$PORT" ]
+then
+    export PORT=3101
+fi
 
 crossbar start &
 gunicorn Server.wsgi --bind 0.0.0.0:3100 --preload --workers=1 --threads=2 --max-requests=400 --max-requests-jitter=100
